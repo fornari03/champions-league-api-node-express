b6b3d235341bfb71400d2c66e9baf2a271c734ab
fornari03 <221017023@aluno.unb.br>
2025-02-23 15:07:41 -0300

    feat: começa o server e o app

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import express, { Request, Response, json } from "express";
+import createApp from "./app";
+
+const app = createApp();
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
